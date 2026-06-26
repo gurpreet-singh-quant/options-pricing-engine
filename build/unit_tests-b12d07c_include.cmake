49f0b1ff359ca6f1c542c16ed43978903eb828bb
Gurpreet Singh <i-miss-her-2much@users.noreply.github.com>
2026-06-27 00:08:38 +0530

    Remove build artifacts, add .gitignore

--- build/unit_tests-b12d07c_include.cmake
+++
@@ -1,5 +0,0 @@
-if(EXISTS "C:/projects/options pricing engine/build/unit_tests-b12d07c_tests.cmake")
-  include("C:/projects/options pricing engine/build/unit_tests-b12d07c_tests.cmake")
-else()
-  add_test(unit_tests_NOT_BUILT-b12d07c unit_tests_NOT_BUILT-b12d07c)
-endif()
